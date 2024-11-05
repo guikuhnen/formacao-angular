c27d27a46d7513ad5272320affedf22392043253
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2024-11-05 20:59:16 -0300

    Operadores aritméticos.

---
+++ IntroducaoJS/003 - Operadores aritméticos/007 - Lógicos/index.js
@@ -0,0 +1,15 @@
+/*
+  && ( E ), || ( OU ), ! (Negação)
+ */
+
+console.log(1 === 1 && 2 >= 1); // true
+console.log(1 === 1 && 1 >= 2); // false
+
+console.log(1 === 1 || 1 >= 2); // true
+console.log(1 === 2 || 1 >= 2); // false
+
+console.log(1 !== 2); // true
+console.log(2 !== 2); // false
+
+console.log(2 != "2"); // false
+console.log(2 !== "2"); // true
