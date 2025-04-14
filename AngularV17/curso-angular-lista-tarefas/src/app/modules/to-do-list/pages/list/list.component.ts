06fc98fffe5c19e829336911acb7716d57955b52
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2025-04-14 20:57:12 -0300

    Ajustando CSS.

--- AngularV17/curso-angular-lista-tarefas/src/app/modules/to-do-list/pages/list/list.component.ts
+++ AngularV17/curso-angular-lista-tarefas/src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'ListComponent',
   standalone: true,
   imports: [],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
 export class ListComponent {
-
+  public addItem = signal(true);
 }
