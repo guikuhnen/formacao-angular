e187f5de4c8a7d3baa99f426433f9fe0c6438761
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2025-06-10 21:15:42 -0300

    Finalizando aula de template-driven.

--- AngularV17/meu-primeiro-projeto-latest/src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
+++ AngularV17/meu-primeiro-projeto-latest/src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
@@ -1,14 +1,42 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-forms',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './template-driven-forms.component.html',
-  styleUrl: './template-driven-forms.component.scss'
+  styleUrl: './template-driven-forms.component.scss',
 })
 export class TemplateDrivenFormsComponent {
+  public listComidas = signal<Array<{ comida: string; preco: string }>>([
+    {
+      comida: 'Pizza',
+      preco: 'R$ 30,00',
+    },
+    {
+      comida: 'Hambúrguer',
+      preco: 'R$ 20,00',
+    },
+    {
+      comida: 'Salada',
+      preco: 'R$ 15,00',
+    },
+    {
+      comida: 'Sushi',
+      preco: 'R$ 50,00',
+    },
+  ]);
 
+  public onSubmit(form: NgForm): void {
+    // Aqui você pode processar os dados do formulário, como enviar para um servidor
+    if (form.valid) {
+      console.log('Dados do formulário:', form.value);
+      // Limpar o formulário após o envio
+      form.resetForm();
+    } else {
+      console.error('Formulário inválido');
+    }
+  }
 }
