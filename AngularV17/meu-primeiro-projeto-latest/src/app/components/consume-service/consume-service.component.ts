6933a26603b426d3243b799050511c866d980574
Guilherme Kuhnen <guijkuhnen@hotmail.com>
2025-07-17 09:14:50 -0300

    Tratativas de erros.

--- AngularV17/meu-primeiro-projeto-latest/src/app/components/consume-service/consume-service.component.ts
+++ AngularV17/meu-primeiro-projeto-latest/src/app/components/consume-service/consume-service.component.ts
@@ -10,6 +10,7 @@ import {
 import { toSignal } from '@angular/core/rxjs-interop';
 import { NewComponent } from '@components/new-component/new-component.component';
 import { ApiService } from 'app/services/api.service';
+import { concatMap } from 'rxjs';
 
 @Component({
   selector: 'app-consume-service',
@@ -35,9 +36,17 @@ export class ConsumeServiceComponent implements OnInit {
 
   // Hard 3
   //public getTask$ = toSignal(this.#apiService.httpListTasks$());
-  public getListTask: Signal<{ id: string; title: string }[] | null> = this.#apiService.getListTask;
+  public getTaskList = this.#apiService.getTaskList;
+  public getTaskListError = this.#apiService.getTaskListError;
 
-  public getTaskId: Signal<{ id: string; title: string } | null> = this.#apiService.getTaskId;
+  public getTaskId = this.#apiService.getTaskId;
+  public getTaskIdError = this.#apiService.getTaskIdError;
+
+  public getTaskCreateError = this.#apiService.getTaskCreateError;
+
+  public getTaskUpdateError = this.#apiService.getTaskUpdateError;
+
+  public getTaskDeleteError = this.#apiService.getTaskDeleteError;
 
   ngOnInit(): void {
     // New
@@ -53,7 +62,7 @@ export class ConsumeServiceComponent implements OnInit {
     //}, 2000);
 
     // Easy 1
-    //this.#apiService.httpListTasks$().subscribe({
+    //this.#apiService.getTaskList$().subscribe({
     // this.getTask$.subscribe({
     //   next: (tasks) => {
     //     this.getTask.set(tasks);
@@ -63,8 +72,48 @@ export class ConsumeServiceComponent implements OnInit {
     // });
 
     // Hard 3
-    this.#apiService.httpListTasks$().subscribe();
+    this.#apiService.httpTaskList$().subscribe();
 
-    this.#apiService.httpTaskId$('v2nCYaQujcZVGr441sqt').subscribe();
+    // this.#apiService.httpTaskId$('G0NUogLpJMVp7I8NSyV6').subscribe();
+  }
+
+  public httpTaskId(id: string) {
+    return this.#apiService.httpTaskId$(id).subscribe({
+      error: (err) => console.error('Error fetching task by ID:', err),
+    });
+  }
+
+  public httpTaskCreate(title: string) {
+    // OK
+    // return this.#apiService.httpTaskCreate$(title).subscribe({
+    //   next: (task) => this.#apiService.httpTaskList$().subscribe(),
+    //   error: (err) => console.error('Error creating task:', err)
+    // });
+
+    // Melhor
+    return this.#apiService
+      .httpTaskCreate$(title)
+      .pipe(concatMap(() => this.#apiService.httpTaskList$()))
+      .subscribe({
+        error: (err) => console.error('Error creating task:', err),
+      });
+  }
+
+  public httpTaskUpdate(id: string, title: string) {
+    return this.#apiService
+      .httpTaskUpdate$(id, title)
+      .pipe(concatMap(() => this.#apiService.httpTaskList$()))
+      .subscribe({
+        error: (err) => console.error('Error updating task:', err),
+      });
+  }
+
+  public httpTaskDelete(id: string) {
+    return this.#apiService
+      .httpTaskDelete$(id)
+      .pipe(concatMap(() => this.#apiService.httpTaskList$()))
+      .subscribe({
+        error: (err) => console.error('Error deleting task:', err),
+      });
   }
 }
--- AngularV17/meu-primeiro-projeto-latest/src/app/services/api.service.ts
+++ AngularV17/meu-primeiro-projeto-latest/src/app/services/api.service.ts
@@ -1,7 +1,14 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from 'environments/environment';
-import { BehaviorSubject, Observable, shareReplay, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  Observable,
+  shareReplay,
+  tap,
+  throwError,
+} from 'rxjs';
 
 interface ITask {
   id: string;
@@ -23,34 +30,111 @@ export class ApiService {
   #http = inject(HttpClient);
   #url = signal(environment.apiTask);
 
-  // public httpListTasks$(): Observable<ITask[]> {
+  // GET List
+  // public httpTaskList$(): Observable<ITask[]> {
   //   return this.#http.get<ITask[]>(this.#url()).pipe(shareReplay());
   // }
 
   // Hard 3
-  #setListTask = signal<ITask[] | null>(null);
-  get getListTask() { 
-    return this.#setListTask.asReadonly();
+  #setTaskList = signal<ITask[] | null>(null);
+  get getTaskList() {
+    return this.#setTaskList.asReadonly();
   }
-  public httpListTasks$(): Observable<ITask[]> {
+  #setTaskListError = signal<ITask[] | null>(null);
+  get getTaskListError() {
+    return this.#setTaskListError.asReadonly();
+  }
+  public httpTaskList$(): Observable<ITask[]> {
+    this.#setTaskList.set(null);
+    this.#setTaskListError.set(null);
+
     return this.#http.get<ITask[]>(this.#url()).pipe(
       shareReplay(),
       tap((res) => {
-        this.#setListTask.set(res);
+        this.#setTaskList.set(
+          res.sort((a, b) => a.title.localeCompare(b.title))
+        );
+      }),
+      catchError((error: HttpErrorResponse) => {
+        this.#setTaskListError.set(error.error.message);
+        return throwError(() => error);
       })
     );
   }
 
+  // GET BY ID
   #setTaskId = signal<ITask | null>(null);
-  get getTaskId() { 
+  get getTaskId() {
     return this.#setTaskId.asReadonly();
   }
+  #setTaskIdError = signal<ITask | null>(null);
+  get getTaskIdError() {
+    return this.#setTaskIdError.asReadonly();
+  }
   public httpTaskId$(id: string): Observable<ITask> {
+    this.#setTaskId.set(null);
+    this.#setTaskIdError.set(null);
+
     return this.#http.get<ITask>(`${this.#url()}/${id}`).pipe(
       shareReplay(),
       tap((res) => {
         this.#setTaskId.set(res);
+      }),
+      catchError((error: HttpErrorResponse) => {
+        this.#setTaskIdError.set(error.error.message);
+        return throwError(() => error);
       })
     );
+  }
+
+  // POST
+  #setTaskCreateError = signal<ITask | null>(null);
+  get getTaskCreateError() {
+    return this.#setTaskCreateError.asReadonly();
+  }
+  public httpTaskCreate$(title: string): Observable<ITask> {
+    this.#setTaskCreateError.set(null);
+
+    return this.#http.post<ITask>(this.#url(), { title }).pipe(
+      shareReplay(),
+      catchError((error: HttpErrorResponse) => {
+        this.#setTaskCreateError.set(error.error.message);
+        return throwError(() => error);
+      })
+    );
+  }
+
+  // Patch
+  #setTaskUpdateError = signal<ITask | null>(null);
+  get getTaskUpdateError() {
+    return this.#setTaskUpdateError.asReadonly();
+  }
+  public httpTaskUpdate$(id: string, title: string): Observable<ITask> {
+    this.#setTaskUpdateError.set(null);
+
+    return this.#http.patch<ITask>(`${this.#url()}/${id}`, { title }).pipe(
+      shareReplay(),
+      catchError((error: HttpErrorResponse) => {
+        this.#setTaskUpdateError.set(error.error.message);
+        return throwError(() => error);
+      })
+    );
+  }
+
+  // Delete
+  #setTaskDeleteError = signal<ITask | null>(null);
+  get getTaskDeleteError() {
+    return this.#setTaskDeleteError.asReadonly();
+  }
+  public httpTaskDelete$(id: string): Observable<void> {
+    this.#setTaskDeleteError.set(null);
+    
+    return this.#http
+      .delete<void>(`${this.#url()}/${id}`, {})
+      .pipe(shareReplay(),
+      catchError((error: HttpErrorResponse) => {
+        this.#setTaskDeleteError.set(error.error.message);
+        return throwError(() => error);
+      }));
   }
 }
